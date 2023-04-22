b5533e6cd7fe3b45df7a368830ac448ea4da8b75
yanliu1111 <liuyan0504@gmail.com>
2023-04-22 16:51:25 -0600

    first api

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restx import Api, Resource
+from config import DevConfig
+
+app = Flask(__name__)
+
+app.config.from_object(DevConfig)
+
+api = Api(app, doc='/docs')
+
+@api.route('/hello')
+class HelloResource(Resource):
+    def get(self):
+        return {'hello': 'world'}
+    
+if __name__ == '__main__':
+    app.run()
+
