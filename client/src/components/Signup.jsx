3f8f7637de54cbae438494e19b13294c3caed899
yanliu1111 <liuyan0504@gmail.com>
2023-04-24 16:43:00 -0600

    feat: simple signup form done

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const SignUpPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassowrd] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const submitForm = () => {
+    console.log("submitting form");
+    console.log(username, email, password, confirmPassword);
+    setUsername("");
+    setEmail("");
+    setPassowrd("");
+    setConfirmPassword("");
+  };
+
   return (
-    <div className="Sign Up">
-      <h1>Sign Up Page</h1>
+    <div className="container">
+      <div className="form">
+        <h1 className="heading">Sign Up</h1>
+        <form action="">
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Username"
+              value={username}
+              name="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              name="password"
+              onChange={(e) => setPassowrd(e.target.value)}
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={confirmPassword}
+              name="confirmPassword"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Button
+              as="sub"
+              variant="primary"
+              type="submit"
+              onClick={submitForm}
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </form>
+      </div>
     </div>
   );
 };
