534c537bab5030babcde6756e27490b8470b4764
yanliu1111 <liuyan0504@gmail.com>
2023-04-26 22:01:12 -0600

    feat: login home logout work

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth, logout } from "../auth";
 
-const HomePage = () => {
+const LoggedInLinks = () => {
+  return (
+    <div className="recipes">
+      <h1>List of Recipes</h1>
+    </div>
+  );
+};
+
+const LoggedOutLinks = () => {
   return (
     <div className="home container">
       <h1 className="heading">Welcome to the Secret Recipes</h1>
@@ -11,6 +20,11 @@ const HomePage = () => {
       </Link>
     </div>
   );
+};
+
+const HomePage = () => {
+  const [logged] = useAuth();
+  return <div>{logged ? <LoggedInLinks /> : <LoggedOutLinks />}</div>;
 };
 
 export default HomePage;
