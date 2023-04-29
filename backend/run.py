8b1df5cf60fcb23d263f1f9e5fb6978d642714b3
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-04-28 20:58:28 -0600

    start docker

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from config import DevConfig
 if __name__ == '__main__':
     app = create_app(DevConfig)
     # app = create_app(ProdConfig)
-    app.run(host="localhost", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
